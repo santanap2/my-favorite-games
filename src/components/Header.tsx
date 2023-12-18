fe22b770784ee6c73828cd5266651849d5a99099
PedroSantana <phsantana99@gmail.com>
2023-12-18 16:32:59 -0300

    fix: header icon fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ import {
 } from '@phosphor-icons/react/dist/ssr'
 import GamesPlatformContext from '@/context/Context'
 import Link from 'next/link'
-import { SlidersHorizontal, UserCircle, X } from '@phosphor-icons/react'
+import { List, SlidersHorizontal, UserCircle, X } from '@phosphor-icons/react'
 import HeaderHooks from '@/hooks/HeaderHooks'
 import { usePathname, useRouter } from 'next/navigation'
 import { games } from '@/data/games'
@@ -70,7 +70,14 @@ export default function Header() {
         onMouseEnter={() => setHoverBtn((prev) => ({ ...prev, menu: true }))}
         onMouseLeave={() => setHoverBtn((prev) => ({ ...prev, menu: false }))}
       >
-        <SlidersHorizontal size={28} weight={menu ? 'duotone' : 'regular'} />
+        {pathname.includes('minha-conta') ? (
+          <List size={28} weight={menu ? 'duotone' : 'regular'} />
+        ) : (
+          <SlidersHorizontal
+            size={28}
+            weight={showMenu.filters ? 'fill' : menu ? 'duotone' : 'regular'}
+          />
+        )}
       </button>
       <div className="w-3/4 flex justify-between items-center xl:w-fit">
         <CSSTransition
@@ -158,7 +165,7 @@ export default function Header() {
               setHoverBtn((prev) => ({ ...prev, user: false }))
             }
           >
-            <span className="uppercase font-semibold text-xs">login</span>
+            <span className="uppercase font-semibold text-xs">Entrar</span>
             <UserCircle size={30} weight={user ? 'duotone' : 'regular'} />
           </Link>
         </div>
