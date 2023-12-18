c1b903688c9aad5fa29f166991e9341ee9d95c3f
PedroSantana <phsantana99@gmail.com>
2023-12-18 19:16:29 -0300

    fix: fix localStorage is not defined error

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -62,26 +62,39 @@ export const currencyMask = (value: string) => {
 }
 
 export const getCartLocalStorage = () => {
-  const cart = JSON.parse(localStorage.getItem('cart') || 'null')
-  return cart
+  if (typeof window !== 'undefined') {
+    const cart = JSON.parse(localStorage.getItem('cart') || 'null')
+    return cart
+  }
 }
 
 export const addToCart = (item: IGame) => {
-  const cart: IGame[] =
-    JSON.parse(localStorage.getItem('cart') || 'false') || []
-  const isItemInCart = cart.some((cartItem) => cartItem.id === item.id)
-  if (isItemInCart) return
+  if (typeof window !== 'undefined') {
+    const cart: IGame[] =
+      JSON.parse(localStorage.getItem('cart') || 'false') || []
+    const isItemInCart = cart.some((cartItem) => cartItem.id === item.id)
+    if (isItemInCart) return
 
-  localStorage.setItem('cart', JSON.stringify([...cart, item]))
+    localStorage.setItem('cart', JSON.stringify([...cart, item]))
+  }
 }
 
-export const addOnlyOneToCart = (item: IGame) =>
-  localStorage.setItem('cart', JSON.stringify([item]))
+export const addOnlyOneToCart = (item: IGame) => {
+  if (typeof window !== 'undefined') {
+    localStorage.setItem('cart', JSON.stringify([item]))
+  }
+}
 
 export const removeFromCart = (id: number) => {
-  const cart = getCartLocalStorage() || []
-  const newCart = cart.filter((item: IGame) => item.id !== id)
-  localStorage.setItem('cart', JSON.stringify(newCart))
+  if (typeof window !== 'undefined') {
+    const cart = getCartLocalStorage() || []
+    const newCart = cart.filter((item: IGame) => item.id !== id)
+    localStorage.setItem('cart', JSON.stringify(newCart))
+  }
 }
 
-export const emptyCart = () => localStorage.setItem('cart', '[]')
+export const emptyCart = () => {
+  if (typeof window !== 'undefined') {
+    localStorage.setItem('cart', '[]')
+  }
+}
