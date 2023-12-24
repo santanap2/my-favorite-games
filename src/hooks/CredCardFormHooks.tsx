6282004a6736f2b130347a6e3cf9c5a5c4105a0e
PedroSantana <phsantana99@gmail.com>
2023-12-24 05:01:27 -0300

    fix: import error fix

--- src/hooks/CredCardFormHooks.tsx
+++ src/hooks/CredCardFormHooks.tsx
@@ -1,6 +1,5 @@
 import { creditCardMask, credCardDateMask, cvvMask } from '@/helpers'
 import { zodResolver } from '@hookform/resolvers/zod'
-import console from 'console'
 import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
