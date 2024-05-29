9db74403c0788b17a80ca8addada21b0ea373eb2
PedroSantana <phsantana99@gmail.com>
2024-05-28 23:41:43 -0300

    update cors

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,6 +4,6 @@ export const api = axios.create({
   baseURL: 'https://gaming-ecommerce-backend.onrender.com',
   withCredentials: true,
   headers: {
-    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin': 'https://myfavgames.netlify.app',
   },
 })
