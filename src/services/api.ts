4590978774a4505e9c26b43b7594f5d6f6b95f45
PedroSantana <phsantana99@gmail.com>
2024-05-28 22:58:23 -0300

    update api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,6 +4,7 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 export const api = axios.create({
-  baseURL: process.env.API_URL,
+  baseURL:
+    process.env.API_URL || 'https://gaming-ecommerce-backend.onrender.com',
   withCredentials: true,
 })
