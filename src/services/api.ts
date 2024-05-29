356caaf132bd522828829f60c8e5d35513f35630
PedroSantana <phsantana99@gmail.com>
2024-05-28 23:58:51 -0300

    update axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,4 @@ import axios from 'axios'
 export const api = axios.create({
   baseURL: 'https://gaming-ecommerce-backend.onrender.com',
   withCredentials: true,
-  headers: {
-    'Access-Control-Allow-Origin': 'https://myfavgames.netlify.app',
-  },
 })
