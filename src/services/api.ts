4b96a523c13cb2fbff6981dad9fcca77cd187ecc
PedroSantana <phsantana99@gmail.com>
2024-05-29 04:16:58 -0300

    fix: change baseurl axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,6 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3003',
-  // baseURL: process.env.API_URL,
+  baseURL: process.env.API_URL,
   withCredentials: true,
 })
