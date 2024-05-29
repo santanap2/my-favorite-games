a36f8405367df0e7cb2e4e79c6e5a5b5a2ae1391
PedroSantana <phsantana99@gmail.com>
2024-05-28 23:35:53 -0300

    update axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,4 +3,7 @@ import axios from 'axios'
 export const api = axios.create({
   baseURL: 'https://gaming-ecommerce-backend.onrender.com',
   withCredentials: true,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+  },
 })
