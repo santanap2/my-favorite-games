f8b7ff577c22b94d9adeb07bac6e63a03af71f63
PedroSantana <phsantana99@gmail.com>
2024-06-19 18:36:44 -0300

    feat: react hook form working fine again

--- src/app/(user)/login/_components/LoginForm.tsx
+++ src/app/(user)/login/_components/LoginForm.tsx
@@ -1,12 +1,47 @@
 'use client'
 
-import LoginHooks from '@/hooks/LoginHooks'
 import Link from 'next/link'
 import React from 'react'
 import { myAction } from '../_actions/login'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 export default function LoginForm() {
-  const { register, errors } = LoginHooks()
+  const formSchema = z.object({
+    email: z
+      .string()
+      .email('Informe um email válido')
+      .min(1, 'Informe seu email'),
+    password: z.string().min(8, 'Informe uma senha válida'),
+    rememberUser: z.boolean(),
+  })
+
+  type FormProps = z.infer<typeof formSchema>
+
+  const {
+    register,
+    formState: { errors },
+    watch,
+  } = useForm<FormProps>({
+    criteriaMode: 'all',
+    mode: 'all',
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  const buttonDisabled = () => {
+    const email = watch('email')
+    const password = watch('password')
+
+    if (!email || !password) return true
+    if (errors.email || errors.password) return true
+
+    return false
+  }
 
   return (
     <form
@@ -16,18 +51,18 @@ export default function LoginForm() {
       <label htmlFor="email" className="flex flex-col gap-1">
         <span className="text-sm font-semibold text-slate-100">Email</span>
         <input
-          {...register('login.email')}
+          {...register('email')}
           type="email"
           id="email"
           name="email"
           placeholder="email@exemplo.com"
           className={`${
-            errors.login?.email && 'border border-red-500'
+            errors.email && 'border border-red-500 border-opacity-70'
           } bg-slate-700 h-10 w-80 rounded px-3 hover:shadow-lg focus:outline-none focus:shadow-xl shadow text-slate-200 placeholder:text-slate-500`}
         />
-        {errors.login?.email && (
+        {errors.email && (
           <span className="text-sm font-light text-red-500">
-            {errors.login.email.message}
+            {errors.email.message}
           </span>
         )}
       </label>
@@ -43,18 +78,18 @@ export default function LoginForm() {
           </Link>
         </div>
         <input
-          {...register('login.password')}
+          {...register('password')}
           type="password"
           id="password"
           name="password"
           placeholder="**********"
           className={`${
-            errors.login?.password && 'border border-red-500'
+            errors.password && 'border border-red-500 border-opacity-70'
           } bg-slate-700 h-10 w-80 rounded px-3 focus:outline-none focus:shadow-xl hover:shadow-lg shadow text-slate-200 placeholder:text-slate-500`}
         />
-        {errors.login?.password && (
+        {errors.password && (
           <span className="text-sm font-light text-red-500">
-            {errors.login.password.message}
+            {errors.password.message}
           </span>
         )}
       </label>
@@ -63,17 +98,14 @@ export default function LoginForm() {
         htmlFor="remember"
         className="flex items-center justify-center gap-2 text-slate-100"
       >
-        <input
-          {...register('login.rememberUser')}
-          type="checkbox"
-          id="remember"
-        />
+        <input {...register('rememberUser')} type="checkbox" id="remember" />
         <span>Lembre-me</span>
       </label>
 
       <button
         type="submit"
         className="w-80 h-10 flex items-center justify-center bg-emerald-500 font-light text-slate-100 rounded text-md shadow hover:bg-emerald-600 disabled:opacity-40 transition-all"
+        disabled={buttonDisabled()}
       >
         Entrar
       </button>
